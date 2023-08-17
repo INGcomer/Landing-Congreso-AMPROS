d42542f541898b40e6b9d8c5edc98e7d1a49dabe
augusto <augustoantonelli88@gmail.com>
2023-08-17 02:24:43 -0300

    agrego el precio para estudiantes

--- src/components/Entrada/Entrada.jsx
+++ src/components/Entrada/Entrada.jsx
@@ -8,67 +8,75 @@ export default function Entrada() {
             <h1> ¿Qué incluye mi entrada? </h1>
             <div className="info">
                 <div className="container">
-                <ul>
-                    <li> <span> Conferencias </span> </li>
-                    <li> <span> 3 Topic </span> </li>
-                    <li> <span> Mesas abierta de Trabajos libres </span> </li>
-                    <li> <span> Simposio de la industria </span> </li>
-                    <li> <span> Feria de empresas </span> </li>
-                </ul>
-                <ul>
-                    <li> <span> Coffees break </span> </li>
-                    <li> <span> Cocktail de bienvenida </span> </li>
-                    <li> <span> Cena de gala </span> </li>
-                    <li> <span> Actividades culturales </span> </li>
-                    <li> <span> Actividades Pre Congreso </span> </li>
-                </ul>
+                    <ul>
+                        <li> <span> Conferencias </span> </li>
+                        <li> <span> 3 Topic </span> </li>
+                        <li> <span> Mesas abierta de Trabajos libres </span> </li>
+                        <li> <span> Simposio de la industria </span> </li>
+                        <li> <span> Feria de empresas </span> </li>
+                    </ul>
+                    <ul>
+                        <li> <span> Coffees break </span> </li>
+                        <li> <span> Cocktail de bienvenida </span> </li>
+                        <li> <span> Cena de gala </span> </li>
+                        <li> <span> Actividades culturales </span> </li>
+                        <li> <span> Actividades Pre Congreso </span> </li>
+                    </ul>
                 </div>
             </div>
 
 
             <div className="NuevasEntradas">
                 <div className="container">
-                <div className="fila">
-                    <div className="nombre destacado">
-                        Entrada General
+                    <div className="fila">
+                        <div className="nombre destacado">
+                            Entrada General
+                        </div>
+                        <div className="precio">
+                            $32.000
+                        </div>
                     </div>
-                    <div className="precio">
-                        $32.000
+                    <div className="fila">
+                        <div className="nombre">
+                            Afiliado AMProS
+                        </div>
+                        <div className="precio">
+                            $22.400
+                        </div>
                     </div>
-                </div>
-                <div className="fila">
-                <div className="nombre">
-                        Afiliado AMProS
+                    <div className="fila">
+                        <div className="nombre">
+                            Asociaciones
+                        </div>
+                        <div className="precio">
+                            $27.200
+                        </div>
                     </div>
-                    <div className="precio">
-                        $22.400
+                    <div className="fila">
+                        <div className="nombre">
+                            Afiliado <br />
+                            <small> AMProS + Asociación </small>
+                        </div>
+                        <div className="precio">
+                            $18.900
+                        </div>
                     </div>
-                </div>
-                <div className="fila">
-                <div className="nombre">
-                        Asociaciones
+                    <div className="fila">
+                        <div className="nombre">
+                            <small> Residentes de especialidades asociadas a salud mental </small>
+                        </div>
+                        <div className="precio">
+                            $16.000
+                        </div>
                     </div>
-                    <div className="precio">
-                        $27.200
+                    <div className="fila">
+                        <div className="nombre">
+                            Estudiantes
+                        </div>
+                        <div className="precio">
+                            $5.000
+                        </div>
                     </div>
-                </div>
-                <div className="fila">
-                <div className="nombre">
-                        Afiliado <br />
-                        <small> AMProS + Asociación </small>
-                    </div>
-                    <div className="precio">
-                        $18.900
-                    </div>
-                </div>
-                <div className="fila">
-                <div className="nombre">
-                        <small> Residentes de especialidades asociadas a salud mental </small>
-                    </div>
-                    <div className="precio">
-                        $16.000
-                    </div>
-                </div>
                 </div>
             </div>
 
