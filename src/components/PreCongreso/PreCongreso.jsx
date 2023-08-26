e16eb5df3bfed3e10e9d1c6a6518e5a2b6dc97c7
augusto <augustoantonelli88@gmail.com>
2023-08-26 16:52:24 -0300

    agrego formularios

--- src/components/PreCongreso/PreCongreso.jsx
+++ src/components/PreCongreso/PreCongreso.jsx
@@ -8,22 +8,28 @@ export default function PreCongreso() {
             <h1> Si solo querés asistir a una actividad del Pre Congreso, podés inscribirte haciendo click en alguno de los siguientes links: </h1>
             <div className="links">
                 <motion.a
-                    href="https://www.withsisa.com/evento/64b9d49f6e8dadd7bc466198"
+                    href="https://forms.gle/gUTNKVWdYYp4yrAp9"
                     whileHover={{ scale: 1.05 }}
                 >
                     Encuentro de la Red Argentina de Enfermería en Salud Mental
                 </motion.a>
                 <motion.a
-                    href="https://www.withsisa.com/evento/64b9d49f6e8dadd7bc466198"
+                    href="https://forms.gle/c9Z5ufCP3xCJeauF6"
                     whileHover={{ scale: 1.05 }}
                 >
                     Encuentro Argentino de Salud Mental y Espiritualidad
                 </motion.a>
                 <motion.a
-                    href="https://www.withsisa.com/evento/64b9d49f6e8dadd7bc466198"
+                    href="https://forms.gle/bvunAYurJrrmJ8kC6"
                     whileHover={{ scale: 1.05 }}
                 >
                     Encuentro Provincial Interdisciplinario de Residentes en Salud Menta
+                </motion.a>
+                <motion.a
+                    href="https://forms.gle/PNMioAsMywG6Ab3g8"
+                    whileHover={{ scale: 1.05 }}
+                >
+                    Conversaciones con Carina Kaplan: Salud Mental y Espiritualidad
                 </motion.a>
             </div>
         </div>
